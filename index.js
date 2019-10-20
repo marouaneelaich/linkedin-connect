2a2b8955a18f0430357986d869f14415df36d69d
Marouane Elaich <marouane.elaich@sablecard.com>
2019-10-20 18:45:34 +0100

    Support headless :boom:

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const argv = require('yargs').argv
 const { init, login, connect } = require('./lib')
 
 const keywords = argv.keywords ? argv.keywords.split(',') : ['']
+const headless = !!argv.headless
 
 async function run() {
-  const { page, browser } = await init()
+  const { page, browser } = await init(headless)
 
   await login(page)
 
