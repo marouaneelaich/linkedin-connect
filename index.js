c042efb8ae0aee9154c0dea5fcf24294b92530e4
Marouane Elaich <marouane.elaich@sablecard.com>
2019-11-04 10:25:45 +0100

    username and password options, making index executable

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+#!/usr/bin/env node
+
 const argv = require('yargs').argv
 const { init, login, connect } = require('./lib')
 
 const keywords = argv.keywords ? argv.keywords.split(',') : ['']
 const headless = !!argv.headless
+const username = argv.username
+const password = argv.password
 
 async function run() {
   const { page, browser } = await init(headless)
 
   console.log('Login in...')
-  await login(page)
+  await login(page, { username, password })
 
   console.log('Preparing...')
   await connect(
